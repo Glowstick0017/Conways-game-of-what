00ff7f8fdff55777bea28abee010d1fed07b7d33
Glowstick // Robbie <robmarracino@gmail.com>
2021-11-13 09:25:04 -0700

    optimize imports and help page text render bug fix

--- src/main/java/com/example/conway/Main.java
+++ src/main/java/com/example/conway/Main.java
@@ -5,8 +5,6 @@ import javafx.application.HostServices;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
